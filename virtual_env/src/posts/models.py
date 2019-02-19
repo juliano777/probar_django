61a1362c98b8cd91c7d21ac92fd4e04ee11fff42
Juliano Atanazio <juliano777@gmail.com>
2019-02-19 14:28:03 -0300

    20190219_1428

--- virtual_env/src/posts/models.py
+++ virtual_env/src/posts/models.py
@@ -1,34 +1,3 @@
-from django.db.models import Model
-from django.db.models import CharField
-from django.db.models import DateTimeField
-from django.db.models import IntegerField
-from django.db.models import TextField
+from django.db import models
 
-
-class Post(Model):
-    _id = IntegerField(db_column='_id', name='_id', primary_key=True,)
-    titulo = CharField(db_column='titulo', name='titulo', max_length=150)
-    titulo = TextField(db_column='titulo', name='titulo',)
-    criado = DateTimeField(db_column='criado', name='criado',)
-    atualizado = DateTimeField(db_column='atualizado', name='atualizado',)
-
-    def __str__(self):
-        return self.titulo
-
-    class Meta:
-        db_table = 'tb_post'
-
-
-class Foo(Model):
-    '''
-    Modelo de tabela para múltiplos schemas no PostgreSQL
-    '''
-
-    _id = IntegerField(db_column='_id', name='_id', primary_key=True,)
-    campo = TextField(db_column='campo', name='campo',)
-
-    def __str__(self):
-        return self.campo
-
-    class Meta:
-        db_table = 'sc_foo"."tb_foo'
+# Create your models here.
