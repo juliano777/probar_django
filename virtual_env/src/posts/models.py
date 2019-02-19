4f55749d17806e73c922b22a7bfbfc505b568b5b
Juliano Atanazio <juliano777@gmail.com>
2019-02-19 12:05:43 -0300

    20190219_1205

--- virtual_env/src/posts/models.py
+++ virtual_env/src/posts/models.py
@@ -1,20 +1,16 @@
 from django.db.models import Model
 from django.db.models import CharField
 from django.db.models import DateTimeField
+from django.db.models import IntegerField
 from django.db.models import TextField
 
 
 class Post(Model):
-    id_ = ''
+    id_ = IntegerField()
+    titulo = CharField()
+    corpo = TextField()
+    criado = DateTimeField()
+    atualizado = DateTimeField()
 
-
-
-'''
-CREATE TABLE tb_post(
-    id_ serial primary key,
-    titulo varchar(150),
-    corpo text,
-    criado timestamp with time zone default now(),
-    atualizado timestamp with time zone default now()
-    );
-'''        
+    def __str__(self):
+        return self.titulo
