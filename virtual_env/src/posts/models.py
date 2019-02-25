95e98087fc1daef6d481b2a390eb85f03d717a39
Juliano Atanazio <juliano777@gmail.com>
2019-02-25 11:22:51 -0300

    20190225_1122

--- virtual_env/src/posts/models.py
+++ virtual_env/src/posts/models.py
@@ -15,7 +15,7 @@ class Post(Model):
     atualizado = DateTimeField(db_column='atualizado', name='atualizado',)
 
     def __str__(self):
-        return '{} - {}'.format('titulo', 'criado')
+        return '{} - {}'.format(self.titulo, 'criado')
 
     class Meta:
         db_table = 'tb_post'
