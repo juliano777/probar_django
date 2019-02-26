a6e58024dba6f73d26a8ad4c98d909f33e1494db
Juliano Atanazio <juliano777@gmail.com>
2019-02-26 12:01:44 -0300

    20190226_1201

--- virtual_env/src/posts/views.py
+++ virtual_env/src/posts/views.py
@@ -8,7 +8,9 @@ def post_detail(request):
     return HttpResponse('<h1>Detail</h1>')
 
 def post_list(request):
-    return HttpResponse('<h1>List</h1>')
+    return render(request, 'index.html')
+
+
 
 def post_update(request):
     return HttpResponse('<h1>Update</h1>')
