9217314ca6fae48a8e22a32e818d14113cf4ea37
Juliano Atanazio <juliano777@gmail.com>
2019-02-26 11:21:06 -0300

    20190226_1121

--- virtual_env/src/posts/urls.py
+++ virtual_env/src/posts/urls.py
@@ -8,9 +8,9 @@ from posts.views import post_delete
 
 
 urlpatterns = (
+               url(r'^list/$|^$', post_list),
                url(r'^create/$', post_create),
                url(r'^detail/$', post_detail),
-               url(r'^list/$', post_list),
                url(r'^update/$', post_update),
                url(r'^delete/$', post_delete),
               )
