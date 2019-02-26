4e9f17705fb31795c4192e5814a29bca909ed8e9
Juliano Atanazio <juliano777@gmail.com>
2019-02-26 07:19:44 -0300

    20190226_0719

--- virtual_env/src/posts/admin.py
+++ virtual_env/src/posts/admin.py
@@ -36,6 +36,36 @@ class PostArrayListFilter(SimpleListFilter):
             queryset = queryset.filter(tags__contains=[lookup_value])
         return queryset
 
+class ListFilter(SimpleListFilter):
+    '''
+    Filtragem de lista baseada nos valores do model Post, campo "tags"
+    '''
+
+    def __init__(self, title, parameter_name):
+        self.title = title
+        self.parameter_name = parameter_name
+
+    def lookups(self, request, model_admin):
+        # Very similar to our code above, but this method must return a
+        # list of tuples: (lookup_value, human-readable value). These
+        # appear in the admin's right sidebar
+
+        tags = Post.objects.values_list('tags', flat=True)
+        tags = [(t, t) for sublist in tags for t in sublist if t]
+        tags = sorted(set(tags))
+        return tags
+
+    def queryset(self, request, queryset):
+        # when a user clicks on a filter, this method gets called. The
+        # provided queryset with be a queryset of Items, so we need to
+        # filter that based on the clicked keyword.
+
+        lookup_value = self.value()  # The clicked keyword. It can be None!
+        if lookup_value:
+            # the __contains lookup expects a list, so...
+            queryset = queryset.filter(tags__contains=[lookup_value])
+        return queryset
+
 class PostModelAdmin(ModelAdmin):
     # Esta classe ModelAdmin serve para utilizarmos as opções de controle de
     # exibição
@@ -53,7 +83,7 @@ class PostModelAdmin(ModelAdmin):
     list_editable = ('tags', )
 
     # Filtro do campo tags
-    list_filter = (PostArrayListFilter, 'criado')
+    list_filter = (ListFilter('foooooooo', 'tags'), 'criado')
 
     # Campos de busca
     # Esse recurso pode ser muito melhor aproveitado no PostgreSQL se
