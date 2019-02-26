b34ee47519a7796bcb365ba765b5c9a1a8aa94a1
Juliano Atanazio <juliano777@gmail.com>
2019-02-26 14:45:53 -0300

    20190226_1445

--- virtual_env/src/posts/actions.py
+++ virtual_env/src/posts/actions.py
@@ -1,1 +1,13 @@
 # Lógica de negócio aqui!!!
+
+from posts.models import Post
+
+
+def get_post_list(request):
+    if request.user.is_authenticated():
+        queryset = Post.objects.all()
+        context = {'titulo': 'LIST - Usuário autenticado', 'lista': queryset}
+    else:
+        context = {'titulo': 'LIST'}
+
+    return context
--- virtual_env/src/posts/views.py
+++ virtual_env/src/posts/views.py
@@ -1,26 +1,25 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-from posts.models import Post
+from posts.actions import get_post_list
+
 
 def post_create(request):
     return HttpResponse('<h1>Create</h1>')
+
 
 def post_detail(request):
     context = {'titulo': 'DETAIL'}
     return render(request, 'index.html', context)
 
-def post_list(request):
-    if request.user.is_authenticated():
-        queryset = Post.objects.all()
-        context = {'titulo': 'LIST - Usuário autenticado', 'lista': queryset}
-    else:
-        context = {'titulo': 'LIST'}
 
-    return render(request, 'index.html', context)
+def post_list(request):
+    return render(request, 'index.html', get_post_list(request))
+
 
 def post_update(request):
     return HttpResponse('<h1>Update</h1>')
+
 
 def post_delete(request):
     return HttpResponse('<h1>Delete</h1>')
