f54b5fb567b3d072630a1e4af9322875fb97129d
Juliano Atanazio <juliano777@gmail.com>
2019-02-26 09:25:28 -0300

    20190226_0925

--- virtual_env/src/posts/views.py
+++ virtual_env/src/posts/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from djano.shortcuts import render
 
-# Create your views here.
-# Requests and responses ONLY!!!
+def post_home(request):
+    return HttpResponse('<h1>Hello, World!!</h1>')
