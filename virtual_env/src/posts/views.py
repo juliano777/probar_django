ba5887da1c6c6293d17a96047c8b94bdf5b53c3a
Juliano Atanazio <juliano777@gmail.com>
2019-03-06 12:47:03 -0300

    20190306_1247

--- virtual_env/src/posts/views.py
+++ virtual_env/src/posts/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from django.shortcuts import render
 
+from posts.actions import get_post_detail
 from posts.actions import get_post_list
 
 
