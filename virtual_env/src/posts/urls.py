27ad25f49ed47bcb2ec99ebcd3746ac934eac185
Juliano Atanazio <juliano777@gmail.com>
2019-03-06 15:29:31 -0300

    20190306_1529

--- virtual_env/src/posts/urls.py
+++ virtual_env/src/posts/urls.py
@@ -1,16 +1,23 @@
 from django.conf.urls import url
 
 from posts.views import post_create
+from posts.views import post_delete
 from posts.views import post_detail
 from posts.views import post_list
 from posts.views import post_update
-from posts.views import post_delete
+
 
 
 urlpatterns = (
+               # List
                url(r'^list/$|^$', post_list),
+               # Create
                url(r'^create/$', post_create),
+               # Detail
                url(r'^detail/(?P<pk>\d+)/$', post_detail),
+               url(r'^(?P<pk>\d+)/$', post_detail),
+               # Update
                url(r'^update/$', post_update),
+               # Delete
                url(r'^delete/$', post_delete),
               )
