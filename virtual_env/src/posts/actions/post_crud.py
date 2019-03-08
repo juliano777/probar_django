1fcf4a87fe460fa232feddc423ed20bc1c79d918
Juliano Atanazio <juliano777@gmail.com>
2019-03-08 13:36:30 -0300

    20190308_1336

--- virtual_env/src/posts/actions/post_crud.py
+++ virtual_env/src/posts/actions/post_crud.py
@@ -29,7 +29,10 @@ def detail(request, pk=None):
 
 def create(request):
     form = PostForm()
+    if request.method == 'POST':
+        print(request.POST)
     context = {'form': form,}
+
     return context
 
 def update(request, pk=None):
