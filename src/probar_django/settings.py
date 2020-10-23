a1cd92a075c762e707915d62dbdefe956121e9c5
Juliano Atanazio <juliano777@gmail.com>
2020-10-23 09:59:49 -0300

    settings.py

--- src/probar_django/settings.py
+++ src/probar_django/settings.py
@@ -73,14 +73,32 @@ WSGI_APPLICATION = 'probar_django.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/1.10/ref/settings/#databases
+# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+
+# Database configuration file location
+DB_CONF_FILE = f'{BASE_DIR}/probar_django/db.conf'
+
+# Read the configurations from file
+DB_CONFIG = ConfigObj(DB_CONF_FILE)
+
+# Database connection parameters
+
+DB_HOST = DB_CONFIG['DB_HOST']
+DB_NAME = DB_CONFIG['DB_NAME']
+DB_USER = DB_CONFIG['DB_USER']
+DB_PASSWORD = DB_CONFIG['DB_PASSWORD']
+DB_PORT = DB_CONFIG['DB_PORT']
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+             'default': {
+                         'ENGINE': 'django.db.backends.postgresql',
+                         'NAME': DB_NAME,
+                         'USER': DB_USER,
+                         'PASSWORD': DB_PASSWORD,
+                         'HOST': DB_HOST,
+                         'PORT': DB_PORT,
+                         }
+            }
 
 
 # Password validation
